3a3fb67cf696912cf085863affa633794355ffb2
shivamkr8227 <63731531+shivamkr8227@users.noreply.github.com>
2023-04-27 17:32:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 // your code here
+let ur=document.getElementById("url");
+
+
+let butt=document.getElementById("button");
+butt.addEventListener("click" , function(){
+	let na=document.getElementById("name").value;
+	let ye=document.getElementById("year").value;
+	if(na==""  && ye== ""){
+		ur.innerHTML="https://localhost:8080/";
+	}
+	else if(na=="" && ye!=""){
+		ur.innerHTML="https://localhost:8080/?year=" + ye;
+	}
+	else if(na!="" && ye==""){
+		ur.innerHTML="https://localhost:8080/?name=" + na;
+	}
+	else{
+		ur.innerHTML="https://localhost:8080/?name=" + na +"&year=" + ye;
+	}
+});
